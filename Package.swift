9107a44cc881f0a0008acdd445c56d76b0345340
cesurbagci <cesurbagci@gmail.com>
2021-07-08 00:00:51 +0300

    package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
+import PackageDescription
+
 let package = Package(
-     name: "Disk",
-     exclude: ["DiskExample", "Tests"]
-     products: [
-         .library(name: "Disk", targets: ["Disk"])
-     ],
-     targets: [
-         .target(name: "Disk", path: "Lib/Sources")
-     ]
- )
+    name: "Disk",
+    exclude: ["DiskExample", "Tests"],
+    products: [
+        .library(name: "Disk", targets: ["Disk"])
+    ],
+    targets: [
+        .target(name: "Disk", path: "Sources")
+    ]
+)
