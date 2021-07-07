bd5d19d71d61beb1771ff9a5bba7cfefd6600cdb
cesurbagci <cesurbagci@gmail.com>
2021-07-08 00:05:49 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,24 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Disk",
-    exclude: ["DiskExample", "Tests"],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "Disk", targets: ["Disk"])
     ],
     targets: [
-        .target(name: "Disk", path: "Sources")
+        .target(
+            name: "Disk",
+            path: "Sources",
+            exclude: ["DiskExample"]
+        ),
+        .testTarget(
+            name: "DiskTests",
+            dependencies: ["Disk"],
+            path: "Tests",
+            exclude:  ["DiskExample"]
+        )
     ]
 )
+
