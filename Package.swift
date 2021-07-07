e9f22824bb9a74f6c4943c48e9da0f9b1bc522d2
cesurbagci <cesurbagci@gmail.com>
2021-07-07 23:59:33 +0300

    package

--- Package.swift
+++ Package.swift
@@ -1,1 +1,10 @@
-.gitroyalty/Package.swift
+let package = Package(
+     name: "Disk",
+     exclude: ["DiskExample", "Tests"]
+     products: [
+         .library(name: "Disk", targets: ["Disk"])
+     ],
+     targets: [
+         .target(name: "Disk", path: "Lib/Sources")
+     ]
+ )
